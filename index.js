a45e78beabb2158f05afc534fe194328fea69e22
orsoli <fljcoli@gmail.com>
2024-08-26 18:37:27 +0200

    added the milestone 1

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 /* Exercise
-    The user clicks on a button that will generate a square game grid.
-    Each cell has a progressive number, from 1 to 100.
-    Therefore, there will be 10 cells for each of the 10 rows.
-    When the user clicks on any cell, the clicked cell turns blue. 
-    And a message with the number of the clicked cell is printed in the console.
+    The computer must generate 16 random numbers within the same range of the chosen difficulty:the bombs.
+    Be careful: only one bomb can be placed in the same cell,
+    so there can’t be two identical numbers in the bomb array.
+    Next, the user clicks on a cell: if the number is present in the list of generated numbers,
+    we have stepped on a bomb. 
+    The cell turns red, and the game ends.Otherwise,
+    the clicked cell turns blue, and the user can continue clicking on the other cells.
+    The game ends when the player clicks on a bomb or when they reach the maximum possible number 
+    of allowed numbers (meaning they have revealed all the cells that are not bombs).
+    At the end of the game, the software must communicate the score, 
+    the number of times the user has clicked on a cell that was not a bomb.
 */
 
 /* Structure
-    M1 - Preapare the HTML and CSS we see in img.
-
-    M2 - Retrieave the interested element from DOM in js 
-       - Create an event when click
-       - Create a loop to create 100 time an element and inseet in DOM in 10 rows and 10 col
-
-    M3 - In each cell, the corresponding number should appear, in order from 1 to 100.
-       - innerText [i + 1]
-
-    M4 - When a cell is clicked, the number of the clicked cell is printed in the console
-    and then the cell is colored blue.
-        - Add an click event for cell element inside loop in form event 
-        - In click event print in concole innerText of cell element
-        - In click event call back addClass function to add the bg-blue class alreaded created in CSS
-
-    BONUS 
-        - Add a select element in DOM with 3 option easy, normal(selected), hard
-        - Created in CSS 3 classes based on selected option
-        - Create a function in JS to check and save the value of the option selected and
-        - Check the options selected and change varables
-        - In event form inside loop check the selected option and add class based on selected option
-
+    M1 - Prepare to keep the user's score. When the user clicks on a cell, we increase the score. 
+        Also the cell can no longer be clicked on.
+            - Create in HTML an element to print the score
+            - In JS retrieve this element to insert the score
+            - In the click cell event create a score variable initialized from 0 and then increment it
+            - Print in page the score
+            - Create in css a class unclicked to do the cell not allowed click
+            - In JS add this cla in cell element
 */
 
 // ---  Preparation Phase
@@ -62,10 +54,16 @@ const gridElement = document.getElementById('grid')
 const selectedElement = document.getElementById('level')
 const btnElement = document.getElementById('play-btn')
 
+// Create a variable initialized
+let score = 0;
+
+
 // --- Procesing Phase
 // Create an event listener in for element
-formElement.addEventListener('submit', function(e){
+formElement.addEventListener('submit',(e) => {
     e.preventDefault() // Blocked the default behavor of form submit
+    gridElement.innerHTML = '' // Clear the elements inside the grid element to ristart the game
+    btnElement.innerText = 'Replay' // Ghange the btn inner text after one click
     // Check the option and change variable
     switch(getSelected(selectedElement)){
         case 'normal':
@@ -81,34 +79,26 @@ formElement.addEventListener('submit', function(e){
             col = 10
             break;
     }
+    // Retrieve root element to give the right value
+    const root = document.querySelector(':root')
+    root.style.setProperty('--cell-per-row', col)
 
     const gridSize = row * col // The size of grid in total number
 
     // Use a for loop to create 100 times a cell element and insert in DOM like gridElement child
     for(let i = 1; i <= gridSize; i++){
         const cellElement = createElement('div')// Create div element
-        // Check selected option to add a class
-        switch(getSelected(selectedElement)){
-            case 'normal':
-                addClass(cellElement,"normal-cell") // Add class='normal-cell' in this div element already created
-                break;
-            case 'hard':
-                addClass(cellElement,"hard-cell") // Add class='hard-cell' in this div element already created
-                break;
-            case 'easy':
-                addClass(cellElement,"easy-cell") // Add class='easy-cell' in this div element already created
-                break;
-        }
+        addClass(cellElement,"cell") // Add class='cell' in this div element already created
         gridElement.appendChild(cellElement) // Append like child new div in the grid element in DOM
         cellElement.innerText = i // Add content in the element cell
-        btnElement.disabled = true
 
         // Add a click event of cell element
-        cellElement.addEventListener('click',function(){
-            // Print in console the inner text of cell element
-            console.log(cellElement.innerText)
-            // Add bg-blue class
-            addClass(cellElement,"bg-blue")
+        cellElement.addEventListener('click',() => {
+            // Retrieve score element for DOM
+            const scoreElement = document.getElementById('score')
+            ++score // Increant score
+            scoreElement.innerText = score // Insert the the content of cell in theis element like inner text
+            addClass(cellElement,"unclicked") // Add bg-blue class
         })
 
     }
